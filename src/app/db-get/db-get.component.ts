1c3b558b81e8b2b36b96593fa90bf6e5729a022e
Kaveri30 <48246050+Kaveri30@users.noreply.github.com>
2019-04-21 20:54:01 +0100

    Added mouse hover to the images
    If the mouse is hovered over an image url, it displays a preview of the image near the mouse.

--- src/app/db-get/db-get.component.ts
+++ src/app/db-get/db-get.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Item from '../Item';
 import { ItemService } from '../item.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-db-get',
@@ -11,9 +12,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DbGetComponent implements OnInit {
 
   items: Item[];
-  adminLoggedIn = window.localStorage.getItem('adminLoggedIn');
+  adminLoggedIn: string = window.localStorage.getItem('adminLoggedIn');
+  popupDiv: HTMLDivElement;
+  popupImage: SafeStyle;
+  mouseX: string;
+  mouseY: string;
 
-  constructor(private is: ItemService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private is: ItemService, private route: ActivatedRoute, private router: Router
+              ,private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     // First check if the admin is logged in.
@@ -49,4 +55,24 @@ export class DbGetComponent implements OnInit {
     }
   }
 
+  // If the mouse enters the table field with the image url, this function is called.
+  mouseEnter(image: string, event: MouseEvent) {
+    // First set the mouse location values
+    this.mouseX = (event.clientX + 30) + 'px';
+    this.mouseY = (event.clientY + 30) + 'px';
+    // Then we want to get the popup div so we can edit it's display type
+    this.popupDiv = document.getElementById('popupDiv') as HTMLDivElement;
+    // Set the popupImage variable to the image URL that was pased in.
+    this.popupImage = image;
+    // Sanitise the URL so it's able to be used for css.
+    this.popupImage = this.sanitizer.bypassSecurityTrustStyle(`url(${image})`);
+    // Set the div to block.
+    this.popupDiv.style.display = 'block';
+ }
+
+ // When the mouse leaves the table field, this function is called.
+ mouseLeave() {
+   // We want to hide the div if the mouse leaves the desired area.
+   this.popupDiv.style.display = 'none';
+ }
 }
